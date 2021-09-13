7634a1af9151f313adda7ed3c9e1037e3941a4ed
Try Ajitiono <aji@tetrate.io>
2021-09-13 17:42:59 +0700

    finish TimeTemplate component
    Signed-off-by: Try Ajitiono <aji@tetrate.io>

---
+++ components/TimeTemplate/TimeTemplate.tsx
@@ -0,0 +1,64 @@
+import styles from './Timeplate.module.css';
+
+interface TimeTemplateProps {
+  years: number;
+  months: number;
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
+export function TimeTemplate({
+  years,
+  months,
+  days,
+  hours,
+  minutes,
+  seconds
+}: TimeTemplateProps) {
+  return (
+    <>
+      <div className={styles.row}>
+        <div className={styles.wrapper}>
+          <div className={styles.value} id="years">
+            {years}
+          </div>
+          <div className={styles.unit}>years</div>
+        </div>
+        <div className={styles.wrapper}>
+          <div className={styles.value} id="months">
+            {months}
+          </div>
+          <div className={styles.unit}>months</div>
+        </div>
+        <div className={styles.wrapper}>
+          <div className={styles.value} id="days">
+            {days}
+          </div>
+          <div className={styles.unit}>days</div>
+        </div>
+      </div>
+      <div className={styles.row}>
+        <div className={styles.wrapper}>
+          <div className={styles.value} id="hours">
+            {hours}
+          </div>
+          <div className={styles.unit}>hours</div>
+        </div>
+        <div className={styles.wrapper}>
+          <div className={styles.value} id="minutes">
+            {minutes}
+          </div>
+          <div className={styles.unit}>minutes</div>
+        </div>
+        <div className={styles.wrapper}>
+          <div className={styles.value} id="seconds">
+            {seconds}
+          </div>
+          <div className={styles.unit}>seconds</div>
+        </div>
+      </div>
+    </>
+  );
+}
