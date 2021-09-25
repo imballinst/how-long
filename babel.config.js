f15ad44589ac448174ab6256a99c3641800af32d
Try Ajitiono <aji@tetrate.io>
2021-09-25 14:42:06 +0700

    fix build warnings
    Signed-off-by: Try Ajitiono <aji@tetrate.io>

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,13 @@
 // babel.config.js
 module.exports = {
   presets: [
+    'next/babel',
     ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-typescript'
+  ],
+  plugins: [
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }]
   ]
 };
