7a1816a646a7b01fa0b36c18de368a76dd5fcb1b
Try Ajitiono <ballinst@gmail.com>
2021-11-23 20:18:02 +0700

    Add comment
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- packages/diary/scripts/generate-collections.ts
+++ packages/diary/scripts/generate-collections.ts
@@ -1,4 +1,5 @@
 import path from 'path';
+// The auto import works because of https://stackoverflow.com/a/68453073.
 import { generateCollection, getAllCollections } from '@how-long/collections';
 
 const PATH_TO_COLLECTIONS = path.join(__dirname, '../collections');
