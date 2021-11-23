a2201161eb02a91f7952205cdee4e0a1d10889d8
Try Ajitiono <ballinst@gmail.com>
2021-11-23 16:07:49 +0700

    separate to folders, we can separate them first
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

---
+++ packages/collections/src/constants.ts
@@ -0,0 +1,6 @@
+export const EFFECTIVE_WIDTHS = ['100%', '30em', '48em', '60em'];
+
+// Social medias.
+export const TWITTER_LINK = 'https://twitter.com/Ajiballinst';
+export const GITHUB_LINK = 'https://github.com/imballinst';
+export const LINKEDIN_LINK = 'https://www.linkedin.com/in/try-ajitiono';
