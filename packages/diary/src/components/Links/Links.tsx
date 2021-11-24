e8422daa50d0b3e3b2e2a8cd64fa8a46bf67afcf
Try Ajitiono <ballinst@gmail.com>
2021-11-24 11:37:53 +0700

    links finished
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- packages/diary/src/components/Links/Links.tsx
+++ packages/diary/src/components/Links/Links.tsx
@@ -1,54 +1,43 @@
-import { PropsWithChildren } from 'react';
-import NextLink, { LinkProps as NextLinkProps } from 'next/link';
-import {
-  Link as ChakraLink,
-  LinkProps as ChakraLinkProps
-} from '@chakra-ui/react';
-import { ReactNode } from 'react';
-import { ExternalLinkIcon } from '@chakra-ui/icons';
+import { AnchorHTMLAttributes, ReactNode } from 'react';
 
-type InternalLinkProps = PropsWithChildren<
-  NextLinkProps & Omit<ChakraLinkProps, 'as'>
->;
-
-export function InternalLink({
-  href,
-  as,
-  replace,
-  scroll,
-  shallow,
-  prefetch,
-  children,
-  ...chakraProps
-}: InternalLinkProps) {
-  return (
-    <NextLink
-      passHref={true}
-      href={href}
-      as={as}
-      replace={replace}
-      scroll={scroll}
-      shallow={shallow}
-      prefetch={prefetch}
-    >
-      <ChakraLink colorScheme="teal" {...chakraProps}>
-        {children}
-      </ChakraLink>
-    </NextLink>
-  );
+export interface LinkProps {
+  href: string;
+  children: ReactNode;
+  isExternal?: boolean;
 }
 
-export function ExternalLink({ children, ...props }: ChakraLinkProps) {
+const ADDITIONAL_EXTERNAL_PROPS = { target: '_blank', rel: 'noopener' };
+
+export function Link({ href, children, isExternal }: LinkProps) {
+  const anchorProps: AnchorHTMLAttributes<HTMLAnchorElement> = {
+    href,
+    className: 'text-teal-500 hover:text-teal-200 underline'
+  };
+  let additionalChildren: ReactNode | undefined;
+
+  if (isExternal) {
+    anchorProps.target = ADDITIONAL_EXTERNAL_PROPS.target;
+    anchorProps.rel = ADDITIONAL_EXTERNAL_PROPS.rel;
+
+    additionalChildren = (
+      <svg viewBox="0 0 24 24" className="ml-4" focusable="false">
+        <g
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-width="2"
+        >
+          <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+          <path d="M15 3h6v6"></path>
+          <path d="M10 14L21 3"></path>
+        </g>
+      </svg>
+    );
+  }
+
   return (
-    <ChakraLink
-      isExternal
-      display="flex"
-      flexDirection="row"
-      alignItems="center"
-      colorScheme="teal"
-      {...props}
-    >
-      {children} <ExternalLinkIcon mx="4px" />
-    </ChakraLink>
+    <a {...anchorProps}>
+      {children} {additionalChildren}
+    </a>
   );
 }
