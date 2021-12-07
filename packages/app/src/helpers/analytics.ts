b78b604f9a3cbe7530118dfc3f83304e797d731c
Try Ajitiono <ballinst@gmail.com>
2021-12-07 07:59:42 +0700

    add analytics, perhaps we need to also create a startup script or something
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

---
+++ packages/app/src/helpers/analytics.ts
@@ -0,0 +1,16 @@
+import { useCallback } from 'react';
+
+// Stub event for local and development stuff.
+function stubEvent(_eventName: string, _eventDetail: any) {}
+function realEvent(eventName: string, eventDetail: any) {
+  gtag('event', eventName, eventDetail);
+}
+
+export function useAnalytics() {
+  return {
+    sendEvent:
+      (import.meta as any).env.PUBLIC_VERCEL_ENV === 'production'
+        ? realEvent
+        : stubEvent
+  };
+}
