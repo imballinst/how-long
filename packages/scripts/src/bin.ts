850a46f005eb113772ee5b87391dc79ce6f7dc40
Try Ajitiono <ballinst@gmail.com>
2021-12-08 07:43:02 +0700

    supply env vars to the init script
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- packages/scripts/src/bin.ts
+++ packages/scripts/src/bin.ts
@@ -1,3 +1,6 @@
+#!/usr/bin/env node
+
+// This file was inspired by https://github.com/isaacs/rimraf/blob/main/bin.js.
 import { execa } from 'execa';
 
 const PATH_TO_APP = new URL('../../app', import.meta.url);
@@ -6,7 +9,10 @@ const [_node, _binjs, command, ...restParams] = process.argv;
 async function main() {
   switch (command) {
     // This works, now let's do the rest.
-    case 'start': {
+    case 'start':
+    case 'dev':
+    case 'build':
+    case 'preview': {
       await execa('yarn', [command, ...restParams], {
         cwd: PATH_TO_APP.pathname,
         stdout: process.stdout,
@@ -18,9 +24,10 @@ async function main() {
       });
       break;
     }
-    case 'dev': {
-      console.log('hello dev');
-      break;
+    default: {
+      console.log(
+        'Invalid command. Available commands are `start`, `dev`, `build`, and `preview`.'
+      );
     }
   }
 }
